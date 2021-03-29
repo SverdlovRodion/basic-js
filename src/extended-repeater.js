82b4ff0e02323c14a354863f6523c48b5ee7daf0
SverdlovRodion <78374115+SverdlovRodion@users.noreply.github.com>
2021-03-29 23:10:42 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -5,3 +5,31 @@ module.exports = function repeater(/* str, options */) {
   // remove line with error and write your code here
 };
   
+
+
+const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, { repeatTimes = 0 , separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = ''}) { 
+  let res = '';
+  res += str;
+  for(let i = 0; i < repeatTimes; i++)
+  {    
+    for(let j = 0; j < additionRepeatTimes; j++)
+    {
+      if(additionRepeatTimes - j == 1)
+        res += addition;
+      else {
+        res += addition + additionSeparator;   
+     }   
+   }
+    if(repeatTimes - i == 1)
+      continue;
+    else{
+      res += separator + str;   
+    }   
+  }
+  if(repeatTimes === 0){
+    return res + addition;
+  }
+  return res;
+  
+};
