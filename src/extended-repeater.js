21d84a2172850f3dc42d0df3529168f65bd75091
SverdlovRodion <78374115+SverdlovRodion@users.noreply.github.com>
2021-03-29 23:12:08 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,13 +1,4 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
-
-
-const CustomError = require("../extensions/custom-error");
 module.exports = function repeater(str, { repeatTimes = 0 , separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = ''}) { 
   let res = '';
   res += str;
