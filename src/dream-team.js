214698604d33f459bb27c3ecdf846c7bfbe56f6d
SverdlovRodion <78374115+SverdlovRodion@users.noreply.github.com>
2021-03-29 22:20:19 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members){
+  
+  let res = '';  
+  if(Object.prototype.toString.call(members) == "[object Array]" ){
+    for(let i in members){
+      let j = 0;
+      if(typeof members[i] == "string"){
+        while(members[i][j] == ' ')
+          j++;
+        res += members[i][j];
+      }
+    }
+  }
+  
+  return res.toUpperCase().split('').sort().join('');
+}
